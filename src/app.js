a3848d71d884f0da6d59c70ba5d6e0d9f4e9b8c2
Gerecs Diána <gerecsdiana@ktch.hu>
2023-03-07 11:30:41 +0100

    idézet beillesztése

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@
 * Copyright: 2023, Gerecs Diána
 * Group: Szoft I-2-E
 * Date: 2023-03-06
-* Github: https://github.com/Gerecs Dia/
+* Github: https://github.com/GerecsDia/
 * Licenc: GNU GPL
 */
 const biketbody = document.querySelector('#biketbody');
@@ -26,19 +26,22 @@ bikeList.forEach((bike) => {
     let tdId = document.createElement('td');
     let tdName = document.createElement('td');
     let tdCol = document.createElement('td');
+    let tdType = document.createElement('td');
     let tdPrice = document.createElement('td');
 
     //Adunk neki tartalmat:
     tdId.textContent = bike.id;
     tdName.textContent = bike.name;
     tdCol.textContent = bike.col;
-    tdPrice.textContent = emp.price;
+    tdType.textContent = bike.type;
+    tdPrice.textContent = bike.price;
 
     //hozzá kell adni a tbodyt a tr és td elemekhez
     biketbody.append(tr);
     tr.append(tdId);
     tr.append(tdName);
     tr.append(tdCol);
+    tr.append(tdType);
     tr.append(tdPrice);
     
 
